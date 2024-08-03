6f117cbaf8cabc760c90cc982c77a15be69a9cec
Arpit Choudhary <arpitchoudharymp41@gmail.com>
2024-08-03 18:14:04 +0530

    Version 1

---
+++ arpit.java
@@ -0,0 +1,24 @@
+public class Arpit
+{
+    public static void main(String[] args)
+    {
+       int i =1;
+
+       while(i<=7)
+       {  
+       
+        System.out.println("Arpit" + i);
+
+         int j = 1;
+         while(j<=1)
+         {
+           
+            System.out.println("vineet " + j);
+            j++;
+         }
+            
+            i++;
+}
+}
+}
+
